a68d9fe71ee8679e1013e7de90b0efb148e7d9bc
suddonm <suddonm@gmail.com>
2025-03-07 13:43:44 -0800

    prime

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public class InterviewPractice
+{
+    public static void Main(string[] args)
+    {
+        List<int> ints = new List<int>() {0, 1, 2, 5, 11, 37, 40};
+
+        foreach (int i in ints)
+        {
+            Console.WriteLine(i + ": " + Prime.IsPrime(i));
+        }
+    }
+}
